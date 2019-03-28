dc50e750f4ffe919adbdac10109101c76b20a32d
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-03-28 09:55:33 -0400

    Add sharpness property to Pencil

--- src/Pencil.js
+++ src/Pencil.js
@@ -1,11 +1,13 @@
 /**
  * A simulation of an ordinary pencil.
  * @constructor
- * @property {number} durability The point durability of the pencil - how many characters it can write before it goes dull.
-  * @property {number} length The length of the pencil - how many times it can be sharpened.
+ * @property {number} sharpness The sharpness of the pencil - how many characters it can write before it goes dull.
+ * @property {number} durability The maximum sharpness of the pencil (which sharpness will be reset to when it is sharpened).
+ * @property {number} length The length of the pencil - how many times it can be sharpened.
  */
-function Pencil() {
-  this.durability = 1000
+function Pencil(durability) {
+  this.durability = durability
+  this.sharpness = 1000
   this.length = 100
 }
 
--- test/Pencil.test.js
+++ test/Pencil.test.js
@@ -11,7 +11,18 @@ describe('Pencil', () => {
     expect(new Pencil()).to.haveOwnProperty('durability')
   })
 
+  it('has a .sharpness property', () => {
+    expect(new Pencil()).to.haveOwnProperty('sharpness')
+  })
+
   it('has a .length property', () => {
     expect(new Pencil()).to.haveOwnProperty('length')
+  })
+
+  describe('durability', () => {
+    it('can be passed as the first argument to the constructor', () => {
+      expect(new Pencil(22).durability).to.equal(22)
+      expect(new Pencil(90).durability).to.equal(90)
+    })
   })
 })
