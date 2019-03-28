1572a2ab153934a8008de4b3fb9f1f5996a61865
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-03-28 10:08:55 -0400

    Document length and durability as parameters

--- src/Pencil.js
+++ src/Pencil.js
@@ -1,9 +1,9 @@
 /**
  * A simulation of an ordinary pencil.
  * @constructor
+ * @param {number} length The length of the pencil - how many times it can be sharpened.
+ * @param {number} durability The maximum sharpness of the pencil (which sharpness will be reset to when it is sharpened).
  * @property {number} sharpness The sharpness of the pencil - how many characters it can write before it goes dull.
- * @property {number} durability The maximum sharpness of the pencil (which sharpness will be reset to when it is sharpened).
- * @property {number} length The length of the pencil - how many times it can be sharpened.
  */
 function Pencil(durability) {
   this.durability = durability
