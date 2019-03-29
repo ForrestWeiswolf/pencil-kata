ef2e262d0e82c4cb881bd93aab88d7cfe6cb4f77
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-03-29 10:27:01 -0400

    Test that Paper starts out blank

--- test/Paper.test.js
+++ test/Paper.test.js
@@ -11,6 +11,10 @@ describe('Paper', () => {
     expect(new Paper()).to.haveOwnProperty('text')
   })
 
+  it('starts out with an empty string as text', () => {
+    expect(new Paper().text).to.equal('')
+  })
+
   describe('Paper.addText', () => {
     it('Is a method', () => {
       expect(new Paper().addText).to.be.a('function')
