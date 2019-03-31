c0940560b8e4373e4f849b6f8c7970105a720fca
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-03-31 11:45:51 -0400

    Refactor and document Paper.addText

--- src/Paper.js
+++ src/Paper.js
@@ -7,17 +7,39 @@ function Paper() {
   this.text = ''
 }
 
-Paper.prototype.addText = function (textToAdd, index) {
-  let overlap = textToAdd.split('').map((charToAdd, i) => {
-    if (this.text[index + i]) {
-      // if both charcters aren't whitespace...
-      return /\S/.test(this.text[index + i]) && /\S/.test(charToAdd) ? '@' : charToAdd
-    } else {
-      return charToAdd
-    }
-  }).join('')
+/* Helper function for Paper.prototype.addText.
+Returns true if passed char is falsy or is whitespace. */
+function isBlank(char) {
+  return !char || /\s/.test(char)
+}
 
-  this.text = this.text.slice(0, index) + overlap + this.text.slice(index + textToAdd.length)
+/* Helper function for Paper.prototype.addText.
+If either argument is blank, returns the other.
+If both are blank, favors the first.
+(The spec didn't say what to do when writing whitespace over different whitespace)
+If neither is blank, returns '@ */
+function overlapChars(charToAdd, charAlreadyThere) {
+  if (isBlank(charToAdd)) {
+    return charAlreadyThere || charToAdd
+  } else {
+    return isBlank(charAlreadyThere) ? charToAdd : '@'
+  }
+}
+
+/** Add text to a sheet of paper.
+ * If added non-whitespace characters overlap preexisting non-whitespace characters,
+ * the paper will have an '@' where the overlap occurred.
+ * @param {textToAdd} [string] - The text to add to this paper
+ * @param {index} [number] - The index at which to start adding that text
+ */
+Paper.prototype.addText = function (textToAdd, index) {
+  const overlap = textToAdd
+    .split('')
+    .map((charToAdd, i) => overlapChars(charToAdd, this.text[index + i]))
+
+  this.text = this.text.slice(0, index) +
+    overlap.join('') +
+    this.text.slice(index + textToAdd.length)
 }
 
 module.exports = Paper
