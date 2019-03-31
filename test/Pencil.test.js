9c4c3511cf8b70ba274f8026fb138ec1d60ea51f
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-03-31 14:52:14 -0400

    Refactor Pencil tests to use beforeEach

--- test/Pencil.test.js
+++ test/Pencil.test.js
@@ -2,21 +2,26 @@ const { expect } = require('chai')
 const { Pencil } = require('../index')
 
 describe('Pencil', () => {
+  let pencil
+  beforeEach(() => {
+    pencil = new Pencil()
+  })
+
   it('Is a constructor', () => {
     expect(Pencil).to.be.a('function')
     expect(new Pencil()).to.be.an('object')
   })
 
   it('has a .durability property', () => {
-    expect(new Pencil()).to.haveOwnProperty('durability')
+    expect(pencil).to.haveOwnProperty('durability')
   })
 
   it('has a .sharpness property', () => {
-    expect(new Pencil()).to.haveOwnProperty('sharpness')
+    expect(pencil).to.haveOwnProperty('sharpness')
   })
 
   it('has a .length property', () => {
-    expect(new Pencil()).to.haveOwnProperty('length')
+    expect(pencil).to.haveOwnProperty('length')
   })
 
   describe('durability', () => {
@@ -26,13 +31,12 @@ describe('Pencil', () => {
     })
 
     it('defaults to 1000', () => {
-      expect(new Pencil().durability).to.equal(1000)
+      expect(pencil.durability).to.equal(1000)
     })
   })
 
   describe('sharpness', () => {
     it('is initially equal to durability', () => {
-      let pencil = new Pencil()
       expect(pencil.durability).to.equal(pencil.sharpness)
       pencil = new Pencil(30)
       expect(pencil.durability).to.equal(pencil.sharpness)
@@ -41,7 +45,6 @@ describe('Pencil', () => {
 
   describe('.write', () => {
     it('is a method', () => {
-      let pencil = new Pencil()
       expect(pencil.write).to.be.a('function')
     })
   })
