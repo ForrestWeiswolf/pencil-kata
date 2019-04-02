c399223f0beae4613043bce6f869f98d99b80506
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-04-02 10:45:58 -0400

    Refactor Pencil.write

--- src/Pencil.js
+++ src/Pencil.js
@@ -17,19 +17,16 @@ Pencil.prototype.write = function (str, paper) {
 
     // Reduce sharpness by 2 if str.toUpperCase() === str, otherwise reduce by 1
     // (Non-letter characters will reduce durability by 1 here)
-    let count = 0
     let isntSpace = /\S/
 
     for (let i = 0; i < str.length; i++) {
       // Comparing str.toLowerCase() to str is significantly faster than using a regex
       if (str[i].toLowerCase() !== str[i]) {
-        count += 2
+        this.sharpness -= 2
       } else if (isntSpace.test(str[i])) {
-        count += 1
+        this.sharpness -= 1
       }
     }
-
-    this.sharpness -= count
   } else {
     paper.addText(new Array(str.length).fill(' ').join(''), paper.text.length)
   }
