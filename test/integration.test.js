213f2c147d49a9e0fa54e4ec1887af2a1f4f5149
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-04-08 12:50:31 -0400

    Add examples from the README as integration tests
    (I did this awhile ago but forgot to commit it)

---
+++ test/integration.test.js
@@ -0,0 +1,50 @@
+const Pencil = require('../src/Pencil')
+const Paper = require('../src/Paper')
+const {
+  expect
+} = require('chai')
+
+describe('Writing', () => {
+  it('Writes on paper', () => {
+    const paper = new Paper()
+    const pencil = new Pencil()
+    pencil.write('She sells sea shells', paper)
+
+    expect(paper.text).to.equal('She sells sea shells')
+  })
+
+  it('Appends to end', () => {
+    const paper = new Paper()
+    const pencil = new Pencil()
+    pencil.write('She sells sea shells', paper)
+    pencil.write(' down by the sea shore', paper)
+    expect(paper.text).to.equal('She sells sea shells down by the sea shore')
+  })
+})
+
+describe('Pencil degradation', () => {
+  it('The pencil can go dull', () => {
+    const paper = new Paper()
+    const pencil = new Pencil(4)
+    pencil.write('Text', paper)
+    expect(paper.text).to.equal('Tex ')
+  })
+})
+
+describe('Erasing', () => {
+  it('Can erase text', () => {
+    const paper = new Paper()
+    const pencil = new Pencil()
+    pencil.write("How much wood would a woodchuck chuck if a woodchuck could chuck wood?", paper)
+    pencil.erase('chuck', paper)
+    expect(paper.text).to.equal("How much wood would a woodchuck chuck if a woodchuck could       wood?")
+  })
+
+  it('Can run out of durability', () => {
+    const paper = new Paper()
+    const pencil = new Pencil(1000, 200, 3)
+    pencil.write('Buffalo Bill', paper)
+    pencil.erase('Bill', paper)
+    expect(paper.text).to.equal('Buffalo B   ')
+  })
+})
